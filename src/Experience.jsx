628d969763639c15bc5aa9099ec0b2776724b943
Hassen David Ortiz <hassen.ortiz@correounivalle.edu.co>
2024-04-26 22:22:42 -0500

    Añadir Vercel
    Se inicializa Vercel en el proyecto para despliegue web.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,14 @@
-const Experience = ({title,subtitle}) => {
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Experience = () => {
+    
     return (
         <>
         <ambientLight/>
         <directionalLight position={[10,10,5]}/>
+        <OrbitControls makeDefault/>
         <mesh>
             <boxGeometry args = {[3,2,3]}/>
             <meshStandardMaterial color = "pink"/>
